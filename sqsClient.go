1420cb52ed0e74f76fee83fd96b067615b5bfc94
Mohammad Zuber Khan <khanz@vmware.com>
2019-07-06 22:47:45 -0700

    Add webserver to handle send and receive messages

--- sqsClient.go
+++ sqsClient.go
@@ -15,7 +15,7 @@ import (
 SetupQueueSession sets up the seesion with the SQS
 
 */
-func SetupQueueSession(queueName string) (*sqs.SQS, *string) {
+func SetupQueueSession() *sqs.SQS {
 	sess, err := session.NewSession(&aws.Config{
 		Region:      aws.String("us-east-1"),
 		Credentials: credentials.NewSharedCredentials("", "test-config"),
@@ -26,9 +26,20 @@ func SetupQueueSession(queueName string) (*sqs.SQS, *string) {
 	}
 
 	svc := sqs.New(sess)
+
+	return svc //, resultURL.QueueUrl
+
+}
+
+/*
+GetQueueURL received the message from the queue specified by the qURL,
+	using the session specified by svc
+*/
+func GetQueueURL(svc *sqs.SQS, queueName string) *string {
 	resultURL, err := svc.GetQueueUrl(&sqs.GetQueueUrlInput{
 		QueueName: aws.String(queueName),
 	})
+
 	if err != nil {
 		if aerr, ok := err.(awserr.Error); ok && aerr.Code() == sqs.ErrCodeQueueDoesNotExist {
 			fmt.Println("Unable to find queue", queueName)
@@ -36,47 +47,34 @@ func SetupQueueSession(queueName string) (*sqs.SQS, *string) {
 		fmt.Println("Unable to queue ", queueName, err)
 
 	}
-	return svc, resultURL.QueueUrl
-
+	return resultURL.QueueUrl
 }
 
 /*
 ReceiveMessage received the message from the queue specified by the qURL,
 	using the session specified by svc
 */
-func ReceiveMessage(svc *sqs.SQS, qURL *string, maxMessages int64) {
+func ReceiveMessage(svc *sqs.SQS, qURL *string, maxMessages int64) (*sqs.ReceiveMessageOutput, error) {
 	result, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
 		QueueUrl:            qURL,
 		MaxNumberOfMessages: aws.Int64(maxMessages),
 		VisibilityTimeout:   aws.Int64(60), // 60 seconds
 		WaitTimeSeconds:     aws.Int64(0),
 	})
-	if err != nil {
-		fmt.Println("Error", err)
-		return
-	}
-	if len(result.Messages) == 0 {
-		fmt.Println("Received no messages")
-		return
-	}
-	fmt.Printf("Success: %+v\n", result.Messages)
+
+	return result, err
 }
 
 /*
 SendMessage sends the message to the queue specified by the qURL
 	using the session specified by svc
 */
-func SendMessage(message string, svc *sqs.SQS, qURL *string) {
+func SendMessage(message string, svc *sqs.SQS, qURL *string) (*sqs.SendMessageOutput, error) {
 	result, err := svc.SendMessage(&sqs.SendMessageInput{
 		DelaySeconds: aws.Int64(10),
 		MessageBody:  aws.String(message),
 		QueueUrl:     qURL,
 	})
+	return result, err
 
-	if err != nil {
-		fmt.Println("Error", err)
-		return
-	}
-
-	fmt.Println("Success", *result.MessageId)
 }
---
+++ webserver.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Message struct {
+	Value string `json:"message"`
+}
+
+func main() {
+	address := "127.0.0.1:8080"
+	log.Println("Starting server on address", address)
+	http.HandleFunc("/message", handle_message)
+	err := http.ListenAndServe(address, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func handle_message(rs http.ResponseWriter, rq *http.Request) {
+	queueName, ok := rq.URL.Query()["queueName"]
+	if !ok || len(queueName[0]) < 1 {
+		log.Println("Url Param 'queueName' is missing")
+		http.Error(rs, "Bad Request", 500)
+		return
+	}
+
+	switch rq.Method {
+	case "GET":
+		HandleReceiveMessage(rs, rq, queueName[0])
+		break
+	case "POST":
+		HandleSendMessage(rs, rq, queueName[0])
+	}
+
+}
+
+func HandleSendMessage(rs http.ResponseWriter, rq *http.Request, queueName string) {
+	message, err := ioutil.ReadAll(rq.Body)
+	defer rq.Body.Close()
+	if err != nil {
+		rs.WriteHeader(http.StatusBadRequest)
+	}
+
+	// Unmarshal
+	var msg Message
+	err = json.Unmarshal(message, &msg)
+	if err != nil {
+		http.Error(rs, err.Error(), 500)
+		return
+	}
+
+	//setup the SQS client session and get the queue URL
+	svc := SetupQueueSession()
+	queueURL := GetQueueURL(svc, queueName)
+
+	result, err := SendMessage(msg.Value, svc, queueURL)
+	if err != nil {
+		http.Error(rs, err.Error(), 500)
+		return
+	}
+
+	log.Println("Message send succeeded")
+	output, err := json.Marshal(result)
+	if err != nil {
+		http.Error(rs, err.Error(), 500)
+		return
+	}
+	rs.WriteHeader(http.StatusCreated)
+	rs.Header().Set("content-type", "application/json")
+	rs.Write(output)
+}
+
+func HandleReceiveMessage(rs http.ResponseWriter, rq *http.Request, queueName string) {
+	//setup the SQS client session and get the queue URL
+	svc := SetupQueueSession()
+	queueURL := GetQueueURL(svc, queueName)
+
+	result, err := ReceiveMessage(svc, queueURL, 10)
+	if err != nil {
+		http.Error(rs, err.Error(), 500)
+		return
+	}
+	log.Println("Messages Received: ", len(result.Messages))
+	output, err := json.Marshal(result)
+	if err != nil {
+		http.Error(rs, err.Error(), 500)
+		return
+	}
+	rs.WriteHeader(http.StatusOK)
+	rs.Header().Set("content-type", "application/json")
+	rs.Write(output)
+}
